94312cd924dde7532a99c117c85f5e61fd355123
Austin Cummings <aac729@live.com>
2020-08-20 18:58:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
   end
   
   def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(open(profile_url))
+    doc = Nokogiri::HTML(open())
     
     link = doc.css('.social-icon-container a').map do |node|
       node.attr('href')
