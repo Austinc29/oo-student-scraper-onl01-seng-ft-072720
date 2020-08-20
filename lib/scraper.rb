5776675053af7ad6db6f5927d76ab4dd13502c71
Austin Cummings <aac729@live.com>
2020-08-20 18:57:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     arr = []
     
-    doc = Nokogiri::HTML(open())
+    doc = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/index.html))
     doc.css('.student-card').each do |card|
       # hash = {}
       # hash[key] = value
