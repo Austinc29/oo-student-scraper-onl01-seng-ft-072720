94ece88d2c2581f3ca70eb3ef6f2be48696b96c6
Austin Cummings <aac729@live.com>
2020-08-20 19:08:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,24 +23,3 @@ class Scraper
   end
   
   def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(open(profile_url))
-    
-    link = doc.css('.social-icon-container a').map do |node|
-      node.attr('href')
-    end
-    
-   # binding.pry
-    #icons
-
-
-    # ex-hash: {
-    #   :twitter=>"http://twitter.com/flatironschool",
-    #   :linkedin=>"https://www.linkedin.com/in/flatironschool",
-    #   :github=>"https://github.com/learn-co",
-    #   :blog=>"http://flatironschool.com",
-    #   :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
-    #   :bio=> "I'm a school"
-    # }
-  end
-
-end
