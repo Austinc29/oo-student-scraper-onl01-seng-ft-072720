56b197c8b3ef1f87804ebcb60f3dad8260b1ef4d
Austin Cummings <aac729@live.com>
2020-08-20 18:52:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url) do 
     student = {}
     index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
+    html = (open(
     scraped_students = Scraper.scrape_index_page(index_url)
     
   end
